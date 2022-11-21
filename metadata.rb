44b86ac0ba1cec2532057eccfb6ba9d53a95cb11
Siddhant Rath <sid@drogon-2129-ilsb.gpws.snet.biobio.tamu.edu>
2022-11-21 11:49:41 -0600

    v1

--- metadata.rb
+++ metadata.rb
@@ -3,17 +3,17 @@ maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures quick_firewall'
-version '0.1.0'
+version '1.0.0'
 chef_version '>= 16.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/quick_firewall/issues'
+issues_url 'https://github.com/SACLabIaaS/quick_firewall/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/quick_firewall'
+source_url 'https://github.com/SACLabIaaS/quick_firewall'
