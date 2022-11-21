14687ff6824bb459e781f8c8085c38f37ded0793
Siddhant Rath <sid@drogon-2129-ilsb.gpws.snet.biobio.tamu.edu>
2022-11-20 19:28:45 -0600

    firewall_install

--- resources/firewall_install.rb
+++ resources/firewall_install.rb
@@ -1,1 +1,29 @@
-# To learn more about Custom Resources, see https://docs.chef.io/custom_resources/
+# This resource would open a firewall port
+unified_mode true
+
+provides :firewall_install,
+          os: 'linux'
+
+action :install do
+  case node['platform']
+  when 'oracle', 'centos', 'fedora'
+    package 'firewalld' do
+      action :install
+    end
+
+    service 'firewalld' do
+      action [ :enable, :start ]
+    end
+
+  when 'ubuntu'
+    package 'ufw' do
+      action :install
+    end
+
+    execute 'enable-ufw' do
+      command 'ufw enable && ufw default allow outgoing && ufw default deny incoming'
+      only_if 'ufw status | grep inactive'
+    end
+  end
+
+end
